887b4a317c5ff0a8cf5236909d96851ac9b87e31
weichengliao11 <75986985+weichengliao11@users.noreply.github.com>
2020-12-26 23:20:22 +0800

    Update p_new.py

--- p_new.py
+++ p_new.py
@@ -2,3 +2,40 @@
 def dictionary():
 	wordlist = ['absolute', 'abstract', 'academic', 'accidental', 'acid', 'adequate', 'admirable', 'aggressive', 'agreeable', 'alert', 'amateur', 'ambitious', 'annual', 'anxious', 'appropriate', 'artificial', 'artistic', 'ashamed', 'associate', 'athletic', 'atomic', 'audio', 'awkward', 'bald', 'bankrupt', 'broke', 'brutal', 'characteristic', 'circular', 'civilian', 'clumsy', 'coarse', 'comic', 'competitive', 'concrete', 'consequent', 'conservative', 'consistent', 'constructive', 'content', 'continual', 'continuous', 'contrary', 'conventional', 'cooperative', 'copper', 'counter', 'courageous', 'courteous', 'critical', 'cunning', 'damp', 'defensible', 'defensive', 'definite', 'delicate', 'delightful', 'dense', 'dependable', 'dependent', 'desperate', 'detective', 'digital', 'distinct', 'distinguished', 'divine', 'dominant', 'durable', 'dusty', 'dynamic', 'earnest', 'economic', 'economical', 'elastic', 'elegant', 'elementary', 'emotional', 'enormous', 'envious', 'essential', 'eventual', 'evident', 'experimental', 'explosive', 'facial', 'faithful', 'fantastic', 'fatal', 'favorable', 'fertile', 'fierce', 'financial', 'flexible', 'fluent', 'fortunate', 'fossil', 'fragrant', 'functional', 'fundamental', 'furious', 'gigantic', 'glorious', 'graceful', 'gracious', 'grammatical', 'grateful', 'grave', 'greasy', 'guilty', 'habitual', 'harsh', 'healthful', 'honorable', 'hopeful', 'identical', 'idle', 'ignorant', 'imaginable', 'imaginary', 'imaginative', 'influential', 'informative', 'initial', 'intellectual', 'intelligent', 'intense', 'intensive', 'intermediate', ' intimate', 'junior', 'keen', 'lawful', 'lean', 'learned', 'leisurely', 'literary', 'logical', 'lousy', 'loyal', 'lunar', 'luxurious', 'magnetic', 'magnificent', 'manual', 'maximum', 'mechanical', 'memorable', 'memorial', 'mere', 'messy', 'mild', 'minimum', 'miserable', 'moderate', 'modest', 'monthly', 'mountainous', 'muddy', 'multiple', 'mutual', 'mysterious', 'nearsighted', 'needy', 'nuclear', 'numerous', 'obedient', 'objective', 'occasional', 'offensive', 'oral', 'organic', 'outstanding', 'oval', 'overnight', 'partial', 'passive', 'peculiar', 'permanent', 'persuasive', 'pessimistic', 'philosophical', 'productive', 'professional', 'profitable', 'prominent', 'promising', 'prompt', 'prosperous', 'psychological', 'realistic', 'relative', 'reluctant', 'remarkable', 'respectable', 'respectful', 'revolutionary', 'rural', 'severe', 'shameful', 'shortsighted', 'singular', 'slight', 'solar', 'sorrowful', 'spare', 'spicy', 'spiritual', 'splendid', 'stingy', 'submarine', 'suspicious', 'tribal', 'troublesome', 'unique', 'universal', 'urban', 'urgent', 'vain', 'vast', 'very', 'virgin', 'visual', 'vital', 'voluntary', 'weekly', 'yearly', 'youthful']
 	return wordlist
+
+alpha_dict = dict()
+alpha_dict["A"] = "a"
+alpha_dict["B"] = "b"
+alpha_dict["C"] = "c"
+alpha_dict["D"] = "d"
+alpha_dict["E"] = "e"
+alpha_dict["F"] = "f"
+alpha_dict["G"] = "g"
+alpha_dict["H"] = "h"
+alpha_dict["I"] = "i"
+alpha_dict["J"] = "j"
+alpha_dict["K"] = "k"
+alpha_dict["L"] = "l"
+alpha_dict["M"] = "m"
+alpha_dict["N"] = "n"
+alpha_dict["O"] = "o"
+alpha_dict["P"] = "p"
+alpha_dict["Q"] = "q"
+alpha_dict["R"] = "r"
+alpha_dict["S"] = "s"
+alpha_dict["T"] = "t"
+alpha_dict["U"] = "u"
+alpha_dict["V"] = "v"
+alpha_dict["W"] = "w"
+alpha_dict["X"] = "x"
+alpha_dict["Y"] = "y"
+alpha_dict["Z"] = "z"
+
+def alphabet_change(alphabet):
+	word = alpha_dict[alphabet]
+	return word
+
+
+
+
+
